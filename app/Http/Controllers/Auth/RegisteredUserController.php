d2fece806372b3abed11bac631f5d8f2c66c244c
salsabilatts <salsabilatts03@gmail.com>
2025-12-11 03:24:58 +0700

    revise redirect register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,8 +44,7 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        return redirect()->route('login')->with('success', 'Account created. Please login.');
 
-        return redirect(RouteServiceProvider::HOME);
     }
 }
